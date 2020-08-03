e9573933bcbd6cc7f62129b3229e3c3b89f017b4
italocc-git <italocc16@gmail.com>
2020-08-03 12:25:27 -0300

    Uma nova categoria implementada

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -39,8 +39,10 @@ function Home() {
 
       <Carousel
         category={dadosIniciais.categorias[5]}
-      />      
-
+      />
+      <Carousel
+        category={dadosIniciais.categorias[6]}
+      />
       <Footer />
     </div>
   );
